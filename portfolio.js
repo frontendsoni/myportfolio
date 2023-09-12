4b46af136219ddbb42707a89c81df9d230d280e5
Gaurav Kumar <frontendsoni@gmail.com>
2023-09-12 13:52:02 +0530

    working with links

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,7 @@
 // ham toggler start here
 let toggleBtn = document.getElementById('ham');
 let showNav = document.getElementById('show-nav');
+let backArrow = document.getElementById('backArrow');
 
 function toggleButton () {
   toggleBtn.classList.toggle("ham");
@@ -14,4 +15,86 @@ toggleBtn.addEventListener('click', () => {
   toggleButton();
   showNavigation();
   
+})
+backArrow.addEventListener('click', () => {
+  toggleButton();
+  showNavigation();
+})
+
+// Toggling companies experience
+ const brimmingGraceLink = document.getElementById('brimmingGraceLink');
+  const lowestRateLink = document.getElementById('lowestRateLink');
+  const brimmingGraceDesc = document.getElementById('brimmingGraceDesc');
+  const lowestRateDesc = document.getElementById('lowestRateDesc');
+
+  function clearActive() {
+    brimmingGraceLink.classList.remove('active-company');
+    lowestRateLink.classList.remove('active-company');
+  }
+  
+  brimmingGraceLink.addEventListener('click', function (e) {
+    e.preventDefault(); 
+    clearActive();
+    brimmingGraceLink.classList.add('active-company');
+    brimmingGraceDesc.style.display = 'block'; 
+    lowestRateDesc.style.display = 'none'; 
+  });
+
+  lowestRateLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    clearActive();
+    lowestRateLink.classList.add('active-company');
+    brimmingGraceDesc.style.display = 'none';
+    lowestRateDesc.style.display = 'block';
+  });
+
+
+  // Fetching data from form and its validations
+
+const contactForm = document.getElementById('contactForm');  
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let nameValue = e.target.name.value;
+  let emailValue = e.target.email.value;
+  let messageValue = e.target.message.value;
+
+  if (nameValue == "") {
+    document.getElementById('nameerr').innerText = 'Please write your full name.';
+    return false
+  } else {
+    document.getElementById('nameerr').innerText = "";
+  }
+  if (emailValue == "") {
+    document.getElementById('emailerr').innerText = 'Please write your email.';
+    return false
+  } else {
+    document.getElementById('emailerr').innerText = "";
+  }
+
+  function isValidEmail(email) {
+    // Regular expression for basic email validation
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+  
+  var email = emailValue;
+  if (!isValidEmail(email)) {
+    document.getElementById('emailerr').innerText = 'Please write a valid email.';
+    return false
+  } else {
+    document.getElementById('emailerr').innerText = "";
+  }
+  
+
+  let data = {
+    name: nameValue,
+    email: emailValue,
+    message: messageValue
+  }
+
+  console.log(data);
+
+  contactForm.reset();
 })
